4ec2abda2d3e98f9711b08e33b9b046be574f755
William Li <williamli@bbi.io>
2018-11-09 13:26:06 +0800

    optmised travis notifications, added notification for deploy stage

---
+++ slack-notifications/travis-before_deploy.js
@@ -0,0 +1,34 @@
+let Slack = require('slack-node');
+
+// console.log("debug: here are some envs...");
+// console.log(process.env);
+
+if(process.env.TRAVIS) {
+
+  webhookUri = "https://hooks.slack.com/services/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";
+   
+  slack = new Slack();
+  slack.setWebhook(webhookUri);
+  
+  if (process.env.TRAVIS_EVENT_TYPE === 'push') {
+    slack.webhook({
+      channel: "#web-dev",
+      icon_emoji: "https://file-gfcrqwuzya.now.sh/travis-ci-female.png",
+      username: "Travis CI",
+      text: `Test completed. Deploying build <${process.env.TRAVIS_BUILD_WEB_URL}|#${process.env.TRAVIS_BUILD_NUMBER} (${process.env.TRAVIS_BUILD_ID})> for <https://github.com/${process.env.TRAVIS_REPO_SLUG}/commit/${process.env.TRAVIS_COMMIT}|commit #${process.env.TRAVIS_COMMIT.substring(0,7)}> @ <https://github.com/${process.env.TRAVIS_REPO_SLUG}|${process.env.TRAVIS_REPO_SLUG}> <https://github.com/${process.env.TRAVIS_REPO_SLUG}/tree/${process.env.TRAVIS_BRANCH}|${process.env.TRAVIS_BRANCH}>: ${process.env.TRAVIS_COMMIT_MESSAGE}`,
+    }, function(err, response) {
+      console.log(response);
+    });
+  } else {
+    slack.webhook({
+      channel: "#web-dev",
+      icon_emoji: "https://file-gfcrqwuzya.now.sh/travis-ci-female.png",
+      username: "Travis CI",
+      text: `Test completed. Deploying for a build trigged by Travis CI event ${process.env.TRAVIS_EVENT_TYPE}`,
+    }, function(err, response) {
+      console.log(response);
+    });
+  }
+  
+
+}
