b91446845b983f1bc8703da83cbd82e6e42636d2
William Li <williamli@bbi.io>
2018-11-09 13:11:16 +0800

    added placeholder notification for other events

--- slack-notifications/travis-started.js
+++ slack-notifications/travis-started.js
@@ -1,7 +1,7 @@
 let Slack = require('slack-node');
 
-console.log("debug: here are some envs...");
-console.log(process.env);
+// console.log("debug: here are some envs...");
+// console.log(process.env);
 
 if(process.env.TRAVIS) {
 
@@ -15,12 +15,19 @@ if(process.env.TRAVIS) {
       channel: "#web-dev",
       icon_emoji: "https://file-gfcrqwuzya.now.sh/travis-ci-female.png",
       username: "Travis CI",
-      text: `Travis CI started working a new build <${process.env.TRAVIS_BUILD_WEB_URL}|#${process.env.TRAVIS_BUILD_NUMBER} (${process.env.TRAVIS_BUILD_ID})> for new commit push <https://github.com/${process.env.TRAVIS_REPO_SLUG}/commit/${process.env.TRAVIS_COMMIT}|#${process.env.TRAVIS_COMMIT}> with <https://github.com/${process.env.TRAVIS_REPO_SLUG}/compare/${process.env.TRAVIS_COMMIT_RANGE}|changes> on <https://github.com/${process.env.TRAVIS_REPO_SLUG}|${process.env.TRAVIS_REPO_SLUG}> <https://github.com/${process.env.TRAVIS_REPO_SLUG}/tree/${process.env.TRAVIS_BRANCH}|${process.env.TRAVIS_BRANCH}>: ${process.env.TRAVIS_COMMIT_MESSAGE}`,
+      text: `Travis CI started working on a new build <${process.env.TRAVIS_BUILD_WEB_URL}|#${process.env.TRAVIS_BUILD_NUMBER} (${process.env.TRAVIS_BUILD_ID})> for new commit push <https://github.com/${process.env.TRAVIS_REPO_SLUG}/commit/${process.env.TRAVIS_COMMIT}|#${process.env.TRAVIS_COMMIT}> with <https://github.com/${process.env.TRAVIS_REPO_SLUG}/compare/${process.env.TRAVIS_COMMIT_RANGE}|changes> on <https://github.com/${process.env.TRAVIS_REPO_SLUG}|${process.env.TRAVIS_REPO_SLUG}> <https://github.com/${process.env.TRAVIS_REPO_SLUG}/tree/${process.env.TRAVIS_BRANCH}|${process.env.TRAVIS_BRANCH}>: ${process.env.TRAVIS_COMMIT_MESSAGE}`,
     }, function(err, response) {
       console.log(response);
     });
   } else {
-
+    slack.webhook({
+      channel: "#web-dev",
+      icon_emoji: "https://file-gfcrqwuzya.now.sh/travis-ci-female.png",
+      username: "Travis CI",
+      text: `Travis CI started working on a new build trigged by Travis CI event ${process.env.TRAVIS_EVENT_TYPE}`,
+    }, function(err, response) {
+      console.log(response);
+    });
   }
   
 
