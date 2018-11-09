a4611084384735ef07a4ead2a931bb72eb304fe5
William Li <williamli@bbi.io>
2018-11-09 13:09:37 +0800

    fixed problems with slack notification payload

--- slack-notifications/travis-started.js
+++ slack-notifications/travis-started.js
@@ -13,9 +13,9 @@ if(process.env.TRAVIS) {
   if (process.env.TRAVIS_EVENT_TYPE === 'push') {
     slack.webhook({
       channel: "#web-dev",
-      icon_url: "https://file-gfcrqwuzya.now.sh/travis-ci-female.png",
+      icon_emoji: "https://file-gfcrqwuzya.now.sh/travis-ci-female.png",
       username: "Travis CI",
-      text: `Travis CI started working a new build <a href="${process.env.TRAVIS_BUILD_WEB_URL}">#${process.env.TRAVIS_BUILD_NUMBER} (${process.env.TRAVIS_BUILD_ID})</a> for new commit push <a href="https://github.com/${process.env.TRAVIS_REPO_SLUG}/commit/${process.env.TRAVIS_COMMIT}">#${process.env.TRAVIS_COMMIT}</a> on <a href="https://github.com/${process.env.TRAVIS_REPO_SLUG}">${process.env.TRAVIS_REPO_SLUG}</a> <a href="https://github.com/${process.env.TRAVIS_REPO_SLUG}/tree/${process.env.TRAVIS_BRANCH}">${process.env.TRAVIS_BRANCH}</a>: ${process.env.TRAVIS_COMMIT_MESSAGE}`,
+      text: `Travis CI started working a new build <${process.env.TRAVIS_BUILD_WEB_URL}|#${process.env.TRAVIS_BUILD_NUMBER} (${process.env.TRAVIS_BUILD_ID})> for new commit push <https://github.com/${process.env.TRAVIS_REPO_SLUG}/commit/${process.env.TRAVIS_COMMIT}|#${process.env.TRAVIS_COMMIT}> with <https://github.com/${process.env.TRAVIS_REPO_SLUG}/compare/${process.env.TRAVIS_COMMIT_RANGE}|changes> on <https://github.com/${process.env.TRAVIS_REPO_SLUG}|${process.env.TRAVIS_REPO_SLUG}> <https://github.com/${process.env.TRAVIS_REPO_SLUG}/tree/${process.env.TRAVIS_BRANCH}|${process.env.TRAVIS_BRANCH}>: ${process.env.TRAVIS_COMMIT_MESSAGE}`,
     }, function(err, response) {
       console.log(response);
     });
