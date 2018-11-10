79f2a59a49599c87526453d1494f58fd357f1557
William Li <williamli@bbi.io>
2018-11-10 13:22:44 +0800

    refine travis before script

--- slack-notifications/travis-before_script.js
+++ slack-notifications/travis-before_script.js
@@ -11,7 +11,7 @@ if(process.env.TRAVIS) {
   slack.setWebhook(webhookUri);
   var text = "";
   if (process.env.TRAVIS_EVENT_TYPE === 'push') {
-    text = `Build <${process.env.TRAVIS_BUILD_WEB_URL}|#${process.env.TRAVIS_BUILD_NUMBER}> (<https://github.com/${process.env.TRAVIS_REPO_SLUG}/compare/${process.env.TRAVIS_COMMIT_RANGE}|${process.env.TRAVIS_COMMIT.substring(0,7)}>) of <https://github.com/${process.env.TRAVIS_REPO_SLUG}|${process.env.TRAVIS_REPO_SLUG}>@<https://github.com/${process.env.TRAVIS_REPO_SLUG}/tree/${process.env.TRAVIS_BRANCH}|${process.env.TRAVIS_BRANCH}> was triggered by <https://github.com/${process.env.TRAVIS_REPO_SLUG}/compare/${process.env.TRAVIS_COMMIT_RANGE}|changes>:\n${process.env.TRAVIS_COMMIT_MESSAGE}`;
+    text = `Build <${process.env.TRAVIS_BUILD_WEB_URL}|#${process.env.TRAVIS_BUILD_NUMBER}> (<https://github.com/${process.env.TRAVIS_REPO_SLUG}/compare/${process.env.TRAVIS_COMMIT_RANGE}|${process.env.TRAVIS_COMMIT.substring(0,7)}>) of <https://github.com/${process.env.TRAVIS_REPO_SLUG}|${process.env.TRAVIS_REPO_SLUG}>@<https://github.com/${process.env.TRAVIS_REPO_SLUG}/tree/${process.env.TRAVIS_BRANCH}|${process.env.TRAVIS_BRANCH}> was triggered by <https://github.com/${process.env.TRAVIS_REPO_SLUG}/compare/${process.env.TRAVIS_COMMIT_RANGE}|new commit>:\n${process.env.TRAVIS_COMMIT_MESSAGE}`;
   } else {
     text = `Build <${process.env.TRAVIS_BUILD_WEB_URL}|#${process.env.TRAVIS_BUILD_NUMBER}> (<https://github.com/${process.env.TRAVIS_REPO_SLUG}/compare/${process.env.TRAVIS_COMMIT_RANGE}|${process.env.TRAVIS_COMMIT.substring(0,7)}>) of ${process.env.TRAVIS_REPO_SLUG}@${process.env.TRAVIS_BRANCH}:\n New build trigged by Travis CI event ${process.env.TRAVIS_EVENT_TYPE}`;
   }
