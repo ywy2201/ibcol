b26f794c6c59d49baec13616f98cb1189e7a8c5c
William Li <williamli@bbi.io>
2018-11-10 19:59:36 +0800

    trying a new approach

--- slack-notifications/now-alias.js
+++ slack-notifications/now-alias.js
@@ -7,8 +7,12 @@ webhookUri = "https://hooks.slack.com/services/TDV04909H/BE0TMGEUE/cATmP0VmP7zIE
 slack = new Slack();
 slack.setWebhook(webhookUri);
 
+const NOW_URL = (process.argv.length > 2) ? process.argv[2] : process.env.NOW_URL;
 
-var text = `Deployment at ${process.env.NOW_URL} has been aliased to ${process.env.ALIAS_PATH}.`;
+const ALIAS_PATH = (process.argv.length > 3) ? process.argv[3] : process.env.ALIAS_PATH;
+
+
+var text = `Deployment at ${NOW_URL} has been aliased to ${ALIAS_PATH}.`;
 
 if(process.env.TRAVIS) {
   text = `Build <${process.env.TRAVIS_BUILD_WEB_URL}|#${process.env.TRAVIS_BUILD_NUMBER}> (<https://github.com/${process.env.TRAVIS_REPO_SLUG}/compare/${process.env.TRAVIS_COMMIT_RANGE}|${process.env.TRAVIS_COMMIT.substring(0,7)}>):\n${text}`;
