e2b6cb0bd493683ed02e757d661505490e2ffe55
Gabriel Chan <gabriel@thinkpanda.com>
2018-12-06 23:41:42 +0800

    Farsi: خوش آمدید

--- translations/fa/index.js
+++ translations/fa/index.js
@@ -2,7 +2,7 @@ module.exports = {
   "_locale":{
     "id":"fa",
     "name":"فارسی",
-    "label":"",
+    "label":"خوش آمدید",
   },
   "_global":require('./_global.json'),
   "_countries": require('./_countries.json'),
