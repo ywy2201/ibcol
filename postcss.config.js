8bcdd80749e9fff7c71cb02d3e4fb70c240aefca
William Li <williamli@bbi.io>
2019-02-27 20:05:34 +0800

    autoprefix support for next-css

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,5 @@
-// let browserLists = [
-//         "last 2 versions",
-//         "IE 10",
-//         "IE 11",
-//         "IE 6",
-//         "Chrome 9",
-//         "Firefox 14"
-//       ];
-// module.exports = (ctx) => ({
-//   plugins: {
-//     "postcss-import": {},
-//     "postcss-normalize": {},
-//     "postcss-cssnext": {},
-//     "postcss-hexrgba": {}
-
-//   }
-// })
+module.exports = () => ({
+  plugins: {
+    autoprefixer: {}
+  }
+})
